8d5e24576e6a6a47186a70dda41299cb0d3ba226
danilo.derosa.dd@gmail.com <danilo.derosa.dd@gmail.com>
2025-07-29 16:47:37 +0200

    Added stacked bar charts

--- docs/stack_charts/js/scales.js
+++ docs/stack_charts/js/scales.js
@@ -2,14 +2,15 @@
 const xScale = d3.scaleBand();
 
 const colorScale = d3.scaleOrdinal();
- 
+
 const defineScales = (data) => {
   // Define the scales domain and range here
   xScale
-    .domain(data.map(d => d.year))
-    .range([0, innerWidth]);
-  
+    .domain(data.map((d) => d.year))
+    .range([0, innerWidth])
+    .paddingInner(0.2);
+
   colorScale
-    .domain(formatsInfo.map(f => f.id))
-    .range(formatsInfo.map(f => f.color));
+    .domain(formatsInfo.map((f) => f.id))
+    .range(formatsInfo.map((f) => f.color));
 };
--- docs/stack_charts/js/stacked-bars.js
+++ docs/stack_charts/js/stacked-bars.js
@@ -1,17 +1,43 @@
 const drawStackedBars = (data) => {
   // Generate the stacked bar chart here
-  
 
   /*******************************/
   /*    Append the containers    */
   /*******************************/
-  const svg = d3.select("#bars")
-  .append("svg")
+  const svg = d3
+    .select("#bars")
+    .append("svg")
     .attr("viewBox", [0, 0, width, height]);
 
   const innerChart = svg
     .append("g")
-      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  
+  const stackGenerator = d3.stack().keys(formatsInfo.map((f) => f.id));
+  const annotatedData = stackGenerator(data);
+  console.log(annotatedData);
+
+  const maxUpperBoundary = d3.max(
+    annotatedData[annotatedData.length - 1],
+    (d) => d[1]
+  );
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, maxUpperBoundary])
+    .range([innerHeight, 0])
+    .nice();
+
+  annotatedData.forEach((series) => {
+    innerChart
+      .selectAll(`.bar-${series.key}`)
+      .data(series)
+      .join("rect")
+      .attr("class", (d) => `.bar-${series.key}`)
+      .attr("x", (d) => xScale(d.data.year))
+      .attr("y", (d) => yScale(d[1]))
+      .attr("width", xScale.bandwidth())
+      .attr("height", (d) => yScale(d[0]) - yScale(d[1]))
+      .attr("fill", colorScale(series.key));
+  });
 };
